824b498488a6043dd2e63e5b01d4ef4e5b041148
Andrew <afellman84@gmail.com>
2022-09-24 10:18:04 -0400

    commit tree 21db8cd8ab5db4aa157661ee3102347aae4a5c64

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+//upperCaseCustomerName(customerName) {
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "barbie"
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "ken"
+}
